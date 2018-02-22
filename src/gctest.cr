98da3daacb173bec611f16cee7b507f6b75c3b54
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-02-22 23:08:42 +0900

    さらに削ったバージョン

--- src/gctest.cr
+++ src/gctest.cr
@@ -1,2 +1,13 @@
 require "./gctest/*"
 require "./*"
+
+puts "start"
+a = Gctest::Observable.range(0, 5)
+a = a.repeat(3)
+ary = a.to_ary
+
+a = Gctest::Observable.from_array [4, 5, 6, 7, 8, 9, 10]
+b = a.filter {|x| x % 2 == 1}
+ary = b.to_ary
+
+puts "end"
