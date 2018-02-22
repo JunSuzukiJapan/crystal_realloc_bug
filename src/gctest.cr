7e52f4faca7e1b8eab81f6b55ff58e00d2ba7d72
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-02-22 23:18:56 +0900

    変数名を変更

--- src/gctest.cr
+++ src/gctest.cr
@@ -6,9 +6,10 @@ puts "start"
 a = Gctest::Observable.range(0, 5)
 a = a.repeat(3)
 ary = a.to_ary
+puts "ary: #{ary}"
 
-a = Gctest::Observable.from_array [4, 5, 6, 7, 8, 9, 10]
-b = a.filter {|x| x % 2 == 1}
+b = Gctest::Observable.from_array [4, 5, 6, 7, 8, 9, 10]
+b = b.filter {|x| x % 2 == 1}
 ary = b.to_ary
 
 puts "end"
